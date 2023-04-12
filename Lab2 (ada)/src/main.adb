4636ad7073612981c804b5f4b704a3aeff96261e
BeshtaNazar <weirddisaster1@gmail.com>
2023-04-12 12:16:53 +0300

    first commit

--- Lab2 (ada)/src/main.adb
+++ Lab2 (ada)/src/main.adb
@@ -4,10 +4,11 @@ procedure Main is
    dim : constant integer := 100000;
    thread_num : constant integer := 6;
 
-   final_min : Integer :=dim;
-   index : Integer := 0;
+
+   final_index : Integer := 0;
 
    arr : array(1..dim) of integer;
+   final_min : Integer :=2147483647;
 
    procedure Init_Arr is
    begin
@@ -16,15 +17,18 @@ procedure Main is
       end loop;
    end Init_Arr;
 
-   function part_min(start_index, finish_index : in integer) return Integer is
-      min : integer := dim;
+   procedure part_min(start_index : in integer; finish_index : in integer; index: out Integer; min: out Integer ) is
+
    begin
+      min := arr(start_index);
+      index:=start_index;
       for i in start_index..finish_index loop
          if arr(i)<min then
             min := arr(i);
+            index:=i;
          end if;
       end loop;
-      return min;
+
    end part_min;
 
    task type starter_thread is
@@ -32,48 +36,51 @@ procedure Main is
    end starter_thread;
 
    protected part_manager is
-      procedure set_part_min(min : in Integer);
-      entry get_min(min : out Integer);
+      procedure set_part_min(min, index : in Integer);
+      entry get_min(min, index: out Integer);
    private
       tasks_count : Integer := 0;
-      part_min : Integer := dim;
+      part_min : Integer := 2147483647;
+      part_index : Integer :=0;
    end part_manager;
 
    protected body part_manager is
-      procedure set_part_min(min : in Integer) is
+      procedure set_part_min(min, index : in Integer) is
       begin
          if part_min>min then
             part_min := min;
+            part_index:= index;
          end if;
 
          tasks_count := tasks_count + 1;
       end set_part_min;
 
-      entry get_min(min : out Integer) when tasks_count = thread_num is
+      entry get_min(min, index : out Integer) when tasks_count = thread_num is
       begin
          min := part_min;
+         index:=part_index;
       end get_min;
 
    end part_manager;
 
    task body starter_thread is
-      min : Integer := dim;
-      start_index, finish_index : Integer;
+      min : Integer := 2147483647;
+      start_index, finish_index, index : Integer;
    begin
       accept start(start_index, finish_index : in Integer) do
          starter_thread.start_index := start_index;
          starter_thread.finish_index := finish_index;
       end start;
-      min := part_min( start_index  => start_index,
-                       finish_index => finish_index);
-      part_manager.set_part_min(min);
+      part_min(start_index, finish_index, index, min);
+      part_manager.set_part_min(min, index);
    end starter_thread;
 
-   function parallel_min return Integer is
-      min : integer := dim;
+   procedure parallel_min (index, min :out Integer) is
       thread : array(1..thread_num) of starter_thread;
       part_dim : integer :=dim/thread_num;
    begin
+      min  := 2147483647;
+      index :=0;
       for i in 1..thread_num loop
          if i=thread_num then
             thread(i).start( part_dim*(i-1)+1, dim);
@@ -81,18 +88,13 @@ procedure Main is
             thread(i).start( part_dim*(i-1)+1, part_dim*i);
          end if;
       end loop;
-      part_manager.get_min(min);
-      return min;
+      part_manager.get_min(min, index);
    end parallel_min;
 
 begin
    Init_Arr;
    arr(dim/3):=-10;
-   final_min:=parallel_min;
-   for i in arr'Range loop
-      if arr(i)=final_min then
-         index :=i;
-      end if;
-   end loop;
-   Put_Line(index'Img&" "&final_min'img);
+   parallel_min(final_index, final_min);
+
+   Put_Line(final_index'Img&" "&final_min'img);
 end Main;
